c0543aca25cd1787a832e830898be7124e9a15a5
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-05 08:49:53 -0300

    commit button and click

--- 06-imc/js/script.js
+++ 06-imc/js/script.js
@@ -1,10 +1,15 @@
 function start() {
-  console.log('start');
-  console.log(calculateImc);
+  var buttonCalculateImc = document.querySelector('#button-calculate-imc');
+  console.log(buttonCalculateImc);
+  buttonCalculateImc.addEventListener('click', handleButtonClick);
 }
 
 function calculateImc(weight, height) {
   return weight / (height * height);
+}
+
+function handleButtonClick() {
+  console.log('cliquei');
 }
 
 start();
