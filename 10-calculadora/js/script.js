20a0361ec39b09c743be6032458f5f9f98790f7d
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-06 16:17:55 -0300

    number

--- 10-calculadora/js/script.js
+++ 10-calculadora/js/script.js
@@ -1,33 +1,42 @@
-var board = [];
 function start() {
-  createBoard();
-
   var botaoCalcular = document.querySelector('#botao-calcular');
   console.log(botaoCalcular);
-  botaoCalcular;
   botaoCalcular.addEventListener('click', handleButtonClick);
 
-  var inputNumeroA = document.querySelector('#input-numero-A');
-  var inputNumeroB = document.querySelector('#input-numero-B');
+  var inputnumeroA = document.querySelector('#input-numero-A');
+  var inputnumeroB = document.querySelector('#input-numero-B');
 
-  inputNumeroA.addEventListener('input', handleButtonClick);
-  inputNumeroB.addEventListener('input', handleButtonClick);
+  inputnumeroA.addEventListener('input', handleButtonClick);
+  inputnumeroB.addEventListener('input', handleButtonClick);
 
   handleButtonClick();
 }
 
-function createBoard() {
-  for (var i=0;)
+var numeroA = Number(inputNumeroA.value);
+var numeroB = Number(inputNumeroB.value);
+
+function somaAB(numeroA, numeroB) {
+  return numeroA + numeroB;
+}
+
+function handleButtonClick() {
+  var inputNumeroA = document.querySelector('#input-numero-A');
+  var inputNumeroB = document.querySelector('#input-numero-B');
+
+  var somaAB = document.querySelector('#somaAB');
+
+  console.log(numeroA);
+  console.log(numeroB);
 }
 
 /* GERANDO UMA FUNÇÃO PARA CALCUALAR SÓ POSIIVOS */
-function NumerosNegativos(inputNumeroA, inputNumeroB) {
+/* function NumerosNegativos(inputNumeroA, inputNumeroB) {
   if (inputNumeroA < 1 || inputNumeroB < 1) {
     console.error(
       'Número inválido, pois é negativo. Digite um número inteiro positivo.'
     );
     return;
   }
-}
+}*/
 
 start();
